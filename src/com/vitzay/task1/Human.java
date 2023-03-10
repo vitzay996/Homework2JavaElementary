eee080673568e68081eb3ae4d06c861b61269a89
Viktor Zaiets <vitzay96@gmail.com>
2023-03-10 18:47:06 +0200

    complete task1

--- src/com/vitzay/task1/Human.java
+++ src/com/vitzay/task1/Human.java
@@ -1,7 +1,34 @@
 package com.vitzay.task1;
 
 public class Human {
+    private String firstName;
     private String lastName;
-    private String FirstName;
     private String middleName;
+
+    public Human (String firstName, String lastName, String middleName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+    }
+
+    public Human(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        if (middleName == null) {
+            return firstName + " " + lastName;
+        } else {
+            return firstName + " " + lastName + " " + middleName;
+        }
+    }
+
+    public String getShortName() {
+        if (middleName == null) {
+            return lastName + " " + firstName.charAt(0) + ".";
+        } else {
+            return lastName + " " + firstName.charAt(0) + ". " + middleName.charAt(0) + ".";
+        }
+    }
 }
--- src/com/vitzay/task1/Main.java
+++ src/com/vitzay/task1/Main.java
@@ -1,6 +1,11 @@
 package com.vitzay.task1;
 public class Main {
     public static void main(String[] args) {
-
+        Human alex = new Human("Alex", "Alexov", "Alexeyev");
+        Human ben = new Human("Ben", "Benov");
+        System.out.println(alex.getFullName());
+        System.out.println(ben.getFullName());
+        System.out.println(alex.getShortName());
+        System.out.println(ben.getShortName());
     }
 }
